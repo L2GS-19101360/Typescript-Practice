c8dcde23d6282ae27649d0ba0e8af96b659b7886
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-12-27 10:55:33 +0800

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import ToDoItemComponent from "./components/todoitemcomponent";
 import { data } from "./data/todoitems";
+import AddToDoForm from "./components/addtodoform";
 
 function App() {
+
+  const[todos, setTodos] = useState(data);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodo) => prevTodo.map(todo => (
+      todo.id === id ? {...todo, completed} : todo
+    )));
+  }
+
+  function addToDo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false
+      },
+      ...prevTodos
+    ])
+  }
 
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Typescript To-Do List</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddToDoForm onSubmit={addToDo}/>
         <div className="space-y-2">
-          {data.map(todoitem => (
-            <ToDoItemComponent todoitem={todoitem} />
+          {todos.map(todoitem => (
+            <ToDoItemComponent todoitem={todoitem}
+            key={todoitem.id}
+            onCompletedChange={setTodoCompleted}  />
           ))}
         </div>
       </div>
--- src/components/todoitemcomponent.tsx
+++ src/components/todoitemcomponent.tsx
@@ -2,13 +2,17 @@ import { ToDoItem } from "../types/todoitem"
 
 interface ToDoItemProps {
     todoitem: ToDoItem;
+    onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function ToDoItemComponent({todoitem}: ToDoItemProps) {
+export default function ToDoItemComponent({todoitem, onCompletedChange}: ToDoItemProps) {
     return (
         <div>
             <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
-                <input type="checkbox" className="scale-125" name="" id="" />
+                <input type="checkbox" 
+                checked={todoitem.completed}
+                onChange={(e) => onCompletedChange(todoitem.id, e.target.checked)}
+                className="scale-125" name="" id="" />
                 <span className={todoitem.completed ? "line-through text-gray-400" : ""}>
                     {todoitem.title}
                 </span>
